28989f1d197f6fed4d5aa6c5aab8fb73d2813908
Josh <josh.holzbach@gmail.com>
2017-11-24 14:34:17 -0500

    CSS tweaks

--- slideshow.php
+++ slideshow.php
@@ -43,7 +43,7 @@
 		<link rel='stylesheet' href='unitegallery/css/unite-gallery.css' type='text/css' />
 	  <script type='text/javascript' src='unitegallery/themes/grid/ug-theme-grid.js'></script>
 	  <link rel='stylesheet' href='unitegallery/themes/default/ug-theme-grid.css' type='text/css' />
-		<link rel='stylesheet' 	href='unitegallery/skins/alexis/alexis.css' type='text/css' />
+		<link rel='stylesheet' href='unitegallery/skins/alexis/alexis.css' type='text/css' />
 	</head>
 
 	<body>
@@ -60,9 +60,9 @@
 			<div id="main">
 					<?php include "navigation.php"; ?>
 
-					<h2 id="slideshowDescription" style='text-align: center; color: white;'><?php echo $title; ?></h2>
+					<h2 id="slideshowDescription" style='font-size: 4rem!important; text-align: center; color: white;'><?php echo $title; ?></h2>
 
-					<div id="gallery" style="margin-bottom: 150px; display:none; margin-left: auto; margin-right: auto;">
+					<div id="gallery" style="margin-top: 45px; margin-bottom: 150px; display:none; margin-left: auto; margin-right: auto;">
 							<?php
 									$query = "SELECT `image`, `caption` FROM `gallery_image` WHERE `gallery_id` = ?";
 									$statement = $db->prepare($query);
@@ -84,9 +84,13 @@
 			$(document).ready(function(){
 					$("#gallery").unitegallery({
 						gallery_theme: "grid",
-						gallery_height: 650,
+						gallery_height: 500,
+						gallery_min_height: 500,
+						thumb_width:120,
+						thumb_height:70,
 						theme_panel_position: "bottom",
 						theme_hide_panel_under_width: null,
+						slider_enable_text_panel: true,
 						slider_enable_progress_indicator:false,
 						slider_enable_play_button:false,
 						slider_enable_fullscreen_button:false,
